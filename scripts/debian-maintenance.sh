3d14fd2bfb9d3c0382a7cc9d591c6132f4f2d6c9
TrudeEH <ehtrude@gmail.com>
2024-01-07 23:06:59 +0000

    Fixed color for m

--- scripts/debian-maintenance.sh
+++ scripts/debian-maintenance.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-echo -e "\e[31m[+] Upgrading...\e[0m"
+echo -e "\e[32m[+] Upgrading...\e[0m"
 sudo apt update
 sudo apt upgrade
 sudo apt dist-upgrade
 sudo apt autoremove
 sudo apt autoclean
 
-echo -e "\e[31m[+] Removing logs older than 7d...\e[0m"
+echo -e "\e[32m[+] Removing logs older than 7d...\e[0m"
 sudo journalctl --vacuum-time=7d
 
-echo -e "\e[31m[i] Cleaning flatpak...:\e[0m"
+echo -e "\e[32m[i] Cleaning flatpak...:\e[0m"
 flatpak remove --unused
 
 read -p "Press enter to exit."
