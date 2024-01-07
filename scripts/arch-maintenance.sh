2813fcd2ce7066145c392a3cccd9444d853db0bd
TrudeEH <ehtrude@gmail.com>
2024-01-07 23:06:59 +0000

    Fixed color for m

--- scripts/arch-maintenance.sh
+++ scripts/arch-maintenance.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-echo -e "\e[31m[+] Upgrading...\e[0m"
+echo -e "\e[32m[+] Upgrading...\e[0m"
 sudo paru -Syu
 
-echo -e "\e[31m[+] Cleaning orphaned (unneeded) packages...\e[0m"
+echo -e "\e[32m[+] Cleaning orphaned (unneeded) packages...\e[0m"
 sudo paru -Rsn $(paru -Qdtq)
 
-echo -e "\e[31m[+] Cleaning old pacman cache...\e[0m"
+echo -e "\e[32m[+] Cleaning old pacman cache...\e[0m"
 paccache -rk1
 
-echo -e "\e[31m[+] Removing logs older than 7d...\e[0m"
+echo -e "\e[32m[+] Removing logs older than 7d...\e[0m"
 sudo journalctl --vacuum-time=7d
 
-echo -e "\e[31m[i] Cleaning flatpak...:\e[0m"
+echo -e "\e[32m[i] Cleaning flatpak...:\e[0m"
 flatpak remove --unused
 
-echo -e "\e[31m[i] AUR Packages installed:\e[0m"
+echo -e "\e[32m[i] AUR Packages installed:\e[0m"
 pacman -Qim | awk '/^Name/{name=$3} /^Installed Size/{print $4$5, name}' | sort -h
 
 # Installed packages: pacman -Qen
