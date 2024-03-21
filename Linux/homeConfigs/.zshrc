ab609a0d9de8d5607084277b0e71e462878d09a4
TrudeEH <ehtrude@gmail.com>
2024-03-21 16:02:35 +0000

    Dotfiles v2.0

--- Linux/homeConfigs/.zshrc
+++
@@ -1,4 +0,0 @@
-source ~/dotfiles/scripts/p.sh
-
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
---
+++ scripts/brewBundle.sh
@@ -0,0 +1,2 @@
+echo "Generate: brew bundle dump --describe"
+echo "Restore: brew bundle --file FileToRestore"
