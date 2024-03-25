938fd9fe5ea39c4e55944c0488f33437de06e7b7
TrudeEH <ehtrude@gmail.com>
2024-03-25 11:26:04 +0000

    Dotfiles rewrite (using stew)
    Full rewrite; Still in testing

--- install.sh
+++ install.sh
@@ -1,32 +1,19 @@
-#! /bin/zsh
+#! /bin/bash
 
-source ./scripts/color.sh
-source ./scripts/symlink-files.sh
-source ./home/.zshrc
+source scripts/p.sh
 
-# Link configs
-echo -e "${GREEN}[+] Symlinking dotfiles...${ENDCOLOR}"
-symlink_files "./home" $HOME
+# ============== CONFIG ==============
 
-echo -n "Run MacOS config script? This will overwrite the system dock. (y/n): "
-read runConfig
-if [[ $runConfig == 'y' ]]; then
-    echo -e "${GREEN}[+] Configuring MacOS...${ENDCOLOR}"
-    bash ./darwinSettings.sh
-fi
+# Install Dependency: stow
+p i stow
 
-# Restore Brew Installs
-echo -e "${GREEN}[+] Restoring Brew from BrewFile...${ENDCOLOR}"
-p b BrewFile
+# Link dotfile home to $HOME
+echo -e "[+] Symlinking dotfiles..."
+stow -v -t $HOME home
 
 # Install Oh-My-ZSH
 echo -e "${GREEN}[+] Installing Oh-My-ZSH...${ENDCOLOR}"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# Update MacOS
-echo -e "${GREEN}[+] Updating MacOS...${ENDCOLOR}"
-echo -e "${GREEN}[I] THE DEVICE WILL RESTART IF NECESSARY.${ENDCOLOR}"
-sudo softwareupdate -iaR
 
 echo -e "${GREEN}${BOLD}[i] All done.${ENDCOLOR}"
