b4186f4579e78538890ba21b771c4dc4c79f08b4
TrudeEH <ehtrude@gmail.com>
2024-04-10 17:25:57 +0000

    Prevent version mismatch error

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ fi
 # ============== HOME MANAGER ==============
 
 # Install
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
 
