f7c8f6e5e3d456d7382cb0cfb1955fb555ac7b13
TrudeEH <ehtrude@gmail.com>
2024-04-10 17:35:38 +0000

    Add default browser settings

--- home.nix
+++ home.nix
@@ -157,6 +157,19 @@
 
   programs.home-manager.enable = true;
 
+  # Default browser
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
+
   programs.firefox = {
     enable = true;
     policies = {
