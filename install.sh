b5b8db46c3b134963101b63991e31967804cd284
TrudeEH <ehtrude@gmail.com>
2024-04-25 20:34:37 +0100

    Use binary instead of compiling chromium

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ fi
 paru -Sy curl git stow bat fzf eza zoxide less nextcloud-client
 
 # Install Apps
-paru -Sy gitg vscodium-bin ollama obsidian signal-desktop fragments fractal secrets newsflash eyedropper obfuscate gnome-console gnome-calendar impression gnome-podcasts geary gnome-builder epiphany ungoogled-chromium
+paru -Sy gitg vscodium-bin ollama obsidian signal-desktop fragments fractal secrets newsflash eyedropper obfuscate gnome-console gnome-calendar impression gnome-podcasts geary gnome-builder epiphany ungoogled-chromium-bin
 
 if [ $(pwd) != "$HOME/dotfiles" ]; then
     cd $HOME
