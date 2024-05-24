f9563145aafd78891cffcba087f542fa0b9c736a
TrudeEH <ehtrude@gmail.com>
2024-05-24 16:40:16 +0100

    Merge debian dotfiles

---
+++ scripts/install-multimc.sh
@@ -0,0 +1,13 @@
+# Install multimc
+sudo apt update
+sudo apt install libqt5core5a libqt5network5 libqt5gui5
+wget https://files.multimc.org/downloads/multimc_1.6-1.deb
+sudo apt install ./multimc_1.6-1.deb
+rm multimc_1.6-1.deb
+
+# Install java
+sudo mkdir -p /etc/apt/keyrings
+wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /etc/apt/keyrings/adoptium.asc
+echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
+sudo apt update
+sudo apt install temurin-8-jdk temurin-21-jdk temurin-17-jdk
