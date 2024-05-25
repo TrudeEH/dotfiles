b81761f8fcdf54a8550464c92da81b026ba69861
TrudeEH <ehtrude@gmail.com>
2024-05-25 11:39:01 +0100

    Add .desktop for running chrome under Wayland

--- scripts/install-steam.sh
+++ scripts/install-steam.sh
@@ -5,5 +5,6 @@ sudoedit /etc/apt/sources.list
 
 sudo dpkg --add-architecture i386
 sudo nala update
+sudo nala install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 sudo nala install steam
 
