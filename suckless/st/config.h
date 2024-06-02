7285780252cd932d90dbc2d8e523e57905176422
TrudeEH <ehtrude@gmail.com>
2024-06-02 22:56:53 +0100

    ST font settings

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrainsMono NF:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
