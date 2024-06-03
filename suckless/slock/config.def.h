4a206bd56847c1ce2bb6c7cb0b33aa1d5b385235
TrudeEH <ehtrude@gmail.com>
2024-06-03 10:50:17 +0100

    Add squares patch

--- suckless/slock/config.def.h
+++ suckless/slock/config.def.h
@@ -3,10 +3,14 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[BG] =     "black",     /* background */
+	[INIT] =   "#4f525c",   /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* size of square in px */
+static const int squaresize = 50;
