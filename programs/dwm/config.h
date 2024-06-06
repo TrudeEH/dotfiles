ecc20d14e623cacad80ebecadb39b0294bcf949e
TrudeEH <ehtrude@gmail.com>
2024-06-07 00:20:51 +0100

    Fix st font

--- programs/dwm/config.h
+++ programs/dwm/config.h
@@ -13,7 +13,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono NF:size=20" };
+static const char *fonts[]          = { "JetBrainsMono NF:size=10" };
 static const char *colors[][3]      = {
 	/*                  fg         bg         border   */
 	[SchemeNorm]  = { "#ebdbb2", "#282828", "#1d2021" },
