b95628edd3f52a16fe52438a83557bb8ef2ebbcd
TrudeEH <ehtrude@gmail.com>
2024-06-07 23:27:01 +0100

    Add screenshot shortcut

--- install.sh
+++ install.sh
@@ -453,7 +453,7 @@ if [[ ${main_menu[@]} =~ 1 ]]; then # DWM
     sudo nala install xorg libx11-dev libxft-dev libxinerama-dev build-essential libxrandr-dev
 
     # Desktop tools
-    sudo nala install feh iwd
+    sudo nala install feh iwd scrot
     sudo cp -f iwd.conf /etc/iwd/main.conf
     sudo systemctl enable iwd
     sudo systemctl start iwd
