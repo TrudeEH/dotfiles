d26762d1c393c18a48f3d1ba7ed4e909b319fa8b
TrudeEH <ehtrude@gmail.com>
2024-06-10 22:31:35 +0100

    Add xorg

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ doas pkg upgrade
 echo "Installing Desktop..."
 
 # ST / DWM / DMENU ...
-doas pkg install x11/libX11 x11-fonts/libXft x11/libXinerama x11/libXext devel/pkgconf print/freetype2 x11-fonts/fontconfig
+doas pkg install xorg x11/libX11 x11-fonts/libXft x11/libXinerama x11/libXext devel/pkgconf print/freetype2 x11-fonts/fontconfig
 
 # SURF
 doas pkg install ftp/curl www/webkit2-gtk3 devel/libsoup security/gcr accessibility/at-spi2-core graphics/cairo graphics/gdk-pixbuf2 devel/glib20 devel/gettext-runtime x11-toolkits/gtk30 x11-toolkits/pango
