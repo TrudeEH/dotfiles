7112eec97e6dd2be516d961bcf090964bddcfa2b
TrudeEH <ehtrude@gmail.com>
2024-06-11 22:20:26 +0100

    Update install.sh script with TUI

--- install.sh
+++ install.sh
@@ -1,83 +1,84 @@
 #! /bin/sh
+BACKTITLE="Trude's FreeBSD Toolkit"
 
-while true; do
-  CHOICE=$(dialog --backtitle "Trude's FreeBSD Toolkit" \
-    --title "Main Menu" \
-    --menu "Select your action:" 15 50 5 \
-      "1" "Update FreeBSD" \
-      "2" "Disk Usage" \
-      "3" "Network Status" \
-      "4" "Exit")
+dialog --erase-on-exit \
+       --backtitle "$BACKTITLE" \
+       --checklist "Use the arrow keys and SPACE to select, then press ENTER." 30 100 5 \
+        "1" "Update FreeBSD" "on"\
+        "2" "Install/Update Dotfiles (DWM)" "on"\
+        "3" "Add Programs" "off" 2> main.tmp
+main_menu=$( cat main.tmp )
+rm main.tmp
 
-  case "$CHOICE" in
-    1)
-      # --- UPDATE FREEBSD ---
-      dialog --gauge "Updating FreeBSD..." 10 50 0  # Initial gauge
+for selection in $main_menu; do
+  if [ "$selection" = "1" ]; then
+  # --- UPDATE FREEBSD ---
 
-      { # Subshell to capture output without displaying it
-        doas freebsd-update fetch install
-        doas pkg update
-        doas pkg upgrade -y
-      } 2>&1 | while read -r line; do
-        # Roughly estimate progress based on keywords
-        case "$line" in
-          *fetch*) PERCENTAGE=20 ;;
-          *install*) PERCENTAGE=50 ;;
-          *upgrad*) PERCENTAGE=80 ;;
-          *complete*) PERCENTAGE=100 ;;
-          *) PERCENTAGE=$((PERCENTAGE + 1)) ;; # Small increments otherwise
-        esac
-        dialog --gauge "$line" 10 50 $PERCENTAGE
-      done
+  showUpdate() {
+    # showUpdate global_per s1 s2 s3 s4
+    dialog --backtitle "$BACKTITLE" --title "Updating FreeBSD and Packages" \
+	       --mixedgauge "Updating..." \
+	       0 0 $1 \
+         "Fetch FreeBSD updates" "$2" \
+         "Update FreeBSD" "$3" \
+         "Fetch package updates" "$4" \
+         "Update packages" "$5"
+    }
 
-      dialog --msgbox "Update complete!" 5 30
-      ;;
-    2)
-      dialog --textbox /proc/mounts 20 60
-      ;;
-    3)
-      dialog --msgbox "$(ip addr | grep 'state UP')" 15 50
-      ;;
-    4)
-      break
-      ;;
-  esac
+    showUpdate 0 7 0 0 0
+
+    doas freebsd-update fetch > install.log
+    showUpdate 20 5 7 0 0
+
+    doas freebsd-update install >> install.log
+    showUpdate 50 5 5 7 0
+
+    doas pkg update >> install.log
+    showUpdate 70 5 5 5 7
+
+    doas pkg upgrade -y >> install.log
+    showUpdate 100 5 5 5 5
+  fi
 done
 
-echo "Installing Desktop..."
+#if [[ ${main_menu[@]} =~ 2 ]]; then
+  #--- INSTALL DOTFILES ---
+
+#fi
 
 # XORG on Intel GPU
-doas pkg install xorg drm-kmod
-doas pw groupmod video -m trude
-doas sysrc kld_list+=i915kms
+
+#doas pkg install xorg drm-kmod
+#doas pw groupmod video -m trude
+#doas sysrc kld_list+=i915kms
 
 # pkg install libva-intel-driver mesa-libs mesa-dri # IF NEEDED ONLY
 
 
 # ST / DWM / DMENU ...
-doas pkg install x11/libX11 x11-fonts/libXft x11/libXinerama x11/libXext devel/pkgconf print/freetype2 x11-fonts/fontconfig
+#doas pkg install x11/libX11 x11-fonts/libXft x11/libXinerama x11/libXext devel/pkgconf print/freetype2 x11-fonts/fontconfig
 
 # SURF
-doas pkg install ftp/curl www/webkit2-gtk3 devel/libsoup security/gcr accessibility/at-spi2-core graphics/cairo graphics/gdk-pixbuf2 devel/glib20 devel/gettext-runtime x11-toolkits/gtk30 x11-toolkits/pango
+#doas pkg install ftp/curl www/webkit2-gtk3 devel/libsoup security/gcr accessibility/at-spi2-core graphics/cairo graphics/gdk-pixbuf2 devel/glib20 devel/gettext-runtime x11-toolkits/gtk30 x11-toolkits/pango
 
 # Utilities
-doas pkg install feh scrot
+#doas pkg install feh scrot
 
-compile() {
-  cd programs/$1
-  doas rm -rf config.h
-  doas make clean install
-  cd ../..
-}
+#compile() {
+#  cd programs/$1
+#  doas rm -rf config.h
+#  doas make clean install
+#  cd ../..
+#}
 
 # Compile programs
-for program in "dwm" "dmenu" "slock" "slstatus" "st" "tabbed" "surf"; do
-  compile $program
-done
+#for program in "dwm" "dmenu" "slock" "slstatus" "st" "tabbed" "surf"; do
+ # compile $program
+#done
 
-doas sysrc dbus_enable=YES #Enable DBUS, needed for Xorg
+#doas sysrc dbus_enable=YES #Enable DBUS, needed for Xorg
 
 
 # Copy dotfiles to $HOME
-cp -vrf $HOME/dotfiles/dotfiles/.* $HOME
+#cp -vrf $HOME/dotfiles/dotfiles/.* $HOME
 
