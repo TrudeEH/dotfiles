e346e989fb1b371fe39ec2f96b83be96f22e0436
TrudeEH <ehtrude@gmail.com>
2024-10-10 00:24:48 +0100

    macOS VM nix-shell

---
+++ nix-shells/macos-vm.nix
@@ -0,0 +1,17 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    quickemu
+  ];
+
+  shellHook = ''
+    mkdir -p macos-vm
+    cd macos-vm
+    quickget macos ventura
+    quickemu --vm macos-ventura.conf
+  '';
+}
